751bf9a8a5df007a1cd154e896e6a71543ae922f
qingjiang_wu <qingjiang_wu@baofu.com>
2019-08-28 20:46:22 +0800

    ''

--- flasky/app/auth/views.py
+++ flasky/app/auth/views.py
@@ -10,21 +10,21 @@ from .forms import LoginForm, RegistrationForm
 from ..email import send_email
 
 
-# @auth.before_app_request
-# def before_request():
-#     if current_user.is_authenticated \
-#             and not current_user.confirmed \
-#             and request.endpoint \
-#             and request.blueprint != 'auth' \
-#             and request.endpoint != 'static':
-#         return redirect(url_for('auth.unconfirmed'))
-#
-#
-# @auth.route('/unconfirmed')
-# def unconfirmed():
-#     if current_user.is_anonymous or current_user.confirmed:
-#         return redirect(url_for('main.index'))
-#     return render_template('auth/unconfirmed.html')
+@auth.before_app_request
+def before_request():
+    if current_user.is_authenticated \
+            and not current_user.confirmed \
+            and request.endpoint \
+            and request.blueprint != 'auth' \
+            and request.endpoint != 'static':
+        return redirect(url_for('auth.unconfirmed'))
+
+
+@auth.route('/unconfirmed')
+def unconfirmed():
+    if current_user.is_anonymous or current_user.confirmed:
+        return redirect(url_for('main.index'))
+    return render_template('auth/unconfirmed.html')
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
